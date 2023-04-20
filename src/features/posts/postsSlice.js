7e99feceee20137e159adf8d8519d516fdf13a65
ATUL TINGRE <atultingre.work@gmail.com>
2023-04-21 01:02:35 +0530

    time ago Added from npm i date-fns

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -7,26 +7,26 @@ const initialState = [
     title: "Learning Redux Toolkit",
     content: "I've heard good things.",
     date: sub(new Date(), { minutes: 10 }).toISOString(),
-    // reactions: {
-    //   thumbsUp: 0,
-    //   wow: 0,
-    //   heart: 0,
-    //   rocket: 0,
-    //   coffee: 0,
-    // },
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
   {
     id: "2",
     title: "Slices...",
     content: "The more I say slice, the more I want pizza.",
     date: sub(new Date(), { minutes: 5 }).toISOString(),
-    // reactions: {
-    //   thumbsUp: 0,
-    //   wow: 0,
-    //   heart: 0,
-    //   rocket: 0,
-    //   coffee: 0,
-    // },
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
 ];
 
@@ -44,11 +44,26 @@ const postsSlice = createSlice({
             id: nanoid(),
             title,
             content,
-            userId
+            date: new Date().toISOString(),
+            userId,
+            reactions: {
+                thumbsUp: 0,
+                wow: 0,
+                heart: 0,
+                rocket: 0,
+                coffee: 0,
+              },
           },
         };
       },
     },
+    reactionAdded(state,action){
+        const {postId, reaction} = action.payload
+        const existingPost = state.find(post => post.id === postId)
+        if (existingPost) {
+            existingPost.reactions[reaction]++
+        }
+    }
   },
 });
 
